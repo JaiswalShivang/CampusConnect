5278a77207f14542db5f621085c1b04fdf079fb3
JaiswalShivang <jaiswalshivang01@gmail.com>
2025-07-03 20:33:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const { socketConnection } = require("./utils/socket");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+  origin: ['https://campusconnect-tau.vercel.app/'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
